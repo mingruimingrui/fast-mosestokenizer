d060ad035165391a535f9e231ccad9ed1ce69ecc
mingruimingrui <mingruimingrui@hotmail.com>
2020-07-18 03:12:57 +0800

    Split building for linux and macos to local and docker

---
+++ build-release-linux.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -e
+
+# Set up environment and work directory
+export CC=clang
+export CXX=clang++
+export DEBIAN_FRONTEND=noninteractive
+
+cd /opt/fast-mosestokenizer
+eval "$(conda shell.bash hook)"
+
+# Download dependencies
+apt update
+apt upgrade -y
+apt install -y clang make cmake meson git curl
+
+# Build dependencies as static libraries
+make download-build-static-deps
+
+# Build and upload packages
+for VERSION in 3.6 3.7 3.8; do
+    conda create -n py$VERSION -y python=$VERSION
+    conda activate py$VERSION
+    python setup.py build_ext bdist_wheel
+    conda deactivate
+done
+
+# # Upload to PyPI
+# conda activate py3.8
+# python -m pip install setuptools wheel twine
+# python -m twine upload dist/*
